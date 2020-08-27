d1828f7fa63d536a813b4b8fb2fd159fde4f741a
Bunna Kal <kal.bunna@gmail.com>
2020-08-27 21:16:05 +0700

    update shop data and add addItemToCart

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,24 +1,20 @@
 import React from 'react';
 
-import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.style.scss';
 
-const CollectinoPreview = ({ title, items }) => (
-
+const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
-    <h1 className='title'>{ title.toUpperCase() }</h1>
-    <div className= 'preview'>
-      {
-        items
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
         .filter((item, idx) => idx < 4)
-        .map(({id, ...otherItemProps}) => (
-          <CollectionItem key = { id } { ...otherItemProps } />
-        ))
-      }
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
   </div>
+);
 
-)
-
-export default CollectinoPreview;
+export default CollectionPreview;
