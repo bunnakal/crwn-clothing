2c9d1d036b534e8d532982dec84eb2fecea135d4
Bunna Kal <kal.bunna@gmail.com>
2020-08-30 11:26:46 +0700

    moved our directory and shop data into its own reducer, created respection ux flow for directory/shop

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,66 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { Link } from 'react-router-dom';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.style.scss';
-import { Link } from 'react-router-dom';
 
-class Directory extends React.Component{
+const Directory = ( { sections } ) => (
 
-  constructor(){
-    super();
+  <div className ='directory-menu'>
+    {
+      sections.map(({id, ...otherSections }) => (
+        <MenuItem key = { id }  {...otherSections}/>
+      ))
+    }
+  </div>
+);
 
-    this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'hats'
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'jackets'
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'sneakers'
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: 'womens'
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-          linkUrl: 'mens'
-        }
-      ]
-    };
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-  }
+export default connect(mapStateToProps)(Directory);
 
-  render() {
-    return(
-      <div className ='directory-menu'>
-        {
-          this.state.sections.map(({id, ...otherSections }) => (
-            <MenuItem key = { id }  {...otherSections}/>
-          ))
-        }
-      </div>
-    );
-  }
-
-}
-
-export default Directory;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 
-import store from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './redux/store';
 
 import './index.css';
 
@@ -15,7 +17,9 @@ import App from './App';
 ReactDOM.render(
   <Provider store = { store }>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor = { persistor } >
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
